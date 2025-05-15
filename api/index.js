e53b1ff3b3d0b9fe90017e4067637ebbf7066818
yuudachi31 <yuudachi31@gmail.com>
2025-05-16 01:46:30 +0800

    fix:firebaseConfig

--- api/index.js
+++ api/index.js
@@ -9,14 +9,24 @@ import ramenPosition from "../json/ramenPosition.json"
 import set from "date-fns/set";
 
 // INITIALIZE FIREBASE
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_FIREBASE_APPID,
+//   // measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID
+// };
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APPID,
+  apiKey: "AIzaSyDNoNyZswwkU7q1cHRfqJ5AvXT3pdRabu4",
+  authDomain: "react-final-e8ba5.firebaseapp.com",
+  databaseURL:"https://react-final-e8ba5-default-rtdb.firebaseio.com",
+  projectId: "react-final-e8ba5",
+  storageBucket: "react-final-e8ba5.firebasestorage.app",
+  messagingSenderId:  "988355603528", 
+  appId: "1:988355603528:web:d7d10972c12d4d962f7c7d"
   // measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID
 };
 
